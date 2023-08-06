078b583d0130b8ade7e129c1838e04f8311ab5b7
Luciano da Silva Martins <zicadopv@gmail.com>
2023-08-06 08:01:36 -0300

    Implementando o modo de criar

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,21 @@ def test_root_deve_retornar_200_e_ola_mundo():
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
